6396fc14e5683f13055669249ff762d435422635
gustavosalm <gustavosalmeida9821@gmail.com>
2021-09-16 00:08:16 -0300

    chance de perder a bola

--- Rhythm Game/Assets/Scripts/MapManager.cs
+++ Rhythm Game/Assets/Scripts/MapManager.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MapManager : MonoBehaviour
-{
+public class MapManager : MonoBehaviour{
     public int ballPosition = 3; // 0, 1, 2, 3, 4, 5, 6
     [SerializeField] private GameObject ball;
+    public bool withEnemy = false;
     private Vector3 centerPos;
     private float deslocamento;
 
-    void Start()
-    {
+    void Start(){
         centerPos = ball.GetComponent<RectTransform>().localPosition;
         deslocamento = ball.transform.parent.GetComponent<RectTransform>().sizeDelta.x / 7;
-        // ball.GetComponent<RectTransform>().localPosition -= new Vector3(deslocamento, 0, 0);
     }
 
-    void Update()
-    {
+    void Update(){
         
     }
 
     public void MoveBall(int movement){
-        ball.GetComponent<RectTransform>().localPosition += new Vector3(deslocamento * movement, 0, 0);
+        float toMove = deslocamento * movement * ((withEnemy) ? -1 : 1);
+        print(toMove);
+        ball.GetComponent<RectTransform>().localPosition += new Vector3(toMove, 0, 0);
+        if(movement == 1){
+            if(Random.Range(0, 100) < 40){
+                if(withEnemy){
+                    withEnemy = false;
+                    ball.GetComponent<Image>().color = new Color32(0, 238, 255, 255);
+                }
+                else{
+                    withEnemy = true;
+                    ball.GetComponent<Image>().color = new Color32(255, 53, 0, 255);
+                }
+            }
+        }
     }
 }
